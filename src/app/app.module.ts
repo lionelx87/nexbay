a2697f6123ce06af6b88bd48f743d615da063af4
Lionel Acosta <lionelx87@gmail.com>
2023-04-11 02:59:49 -0300

    feat(home): icon with call action

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { HttpClientModule } from '@angular/common/http';
+import { AngularSvgIconModule } from 'angular-svg-icon';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    AngularSvgIconModule.forRoot(),
+    SharedModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -3,9 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { AngularSvgIconModule } from 'angular-svg-icon';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [HomePageComponent],
-  imports: [CommonModule, HomeRoutingModule],
+  imports: [
+    CommonModule,
+    HomeRoutingModule,
+    AngularSvgIconModule,
+    ButtonModule,
+  ],
 })
 export class HomeModule {}
