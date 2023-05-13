e34beba1851a7fda6dc2d316b4b1e5dadd3a629a
Lionel Acosta <lionelx87@gmail.com>
2023-05-13 20:56:42 -0300

    feat(general): rename project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,17 +14,17 @@ describe('AppComponent', () => {
   //   const app = fixture.componentInstance;
   //   expect(app).toBeTruthy();
   // });
-  // it(`should have as title 'nexbay'`, () => {
+  // it(`should have as title 'youlist'`, () => {
   //   const fixture = TestBed.createComponent(AppComponent);
   //   const app = fixture.componentInstance;
-  //   expect(app.title).toEqual('nexbay');
+  //   expect(app.title).toEqual('youlist');
   // });
   // it('should render title', () => {
   //   const fixture = TestBed.createComponent(AppComponent);
   //   fixture.detectChanges();
   //   const compiled = fixture.nativeElement as HTMLElement;
   //   expect(compiled.querySelector('.content span')?.textContent).toContain(
-  //     'nexbay app is running!'
+  //     'youlist app is running!'
   //   );
   // });
   it('test', () => {
