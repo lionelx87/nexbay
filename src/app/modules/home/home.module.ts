d892b6c7e981e35a04543225e6a6e8d28c2d18fe
Lionel Acosta <lionelx87@gmail.com>
2023-05-13 18:53:35 -0300

    feat(home): remove extra section

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -4,12 +4,10 @@ import { NgModule } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { AngularSvgIconModule } from 'angular-svg-icon';
 import { ButtonModule } from 'primeng/button';
-import { MenuModule } from 'primeng/menu';
-import { PanelModule } from 'primeng/panel';
 import { RippleModule } from 'primeng/ripple';
+import { ModalsModule } from 'src/app/shared/components/modals/modals.module';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomePageComponent } from './pages/home-page/home-page.component';
-import { ModalsModule } from 'src/app/shared/components/modals/modals.module';
 
 @NgModule({
   declarations: [HomePageComponent],
@@ -20,8 +18,6 @@ import { ModalsModule } from 'src/app/shared/components/modals/modals.module';
     AngularSvgIconModule,
     ButtonModule,
     RippleModule,
-    PanelModule,
-    MenuModule,
     ModalsModule,
   ],
 })
