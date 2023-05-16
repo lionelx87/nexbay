f332781e8f50adf11137119445de1edc3259f6d3
Lionel Acosta <lionelx87@gmail.com>
2023-05-15 21:57:09 -0300

    feat(register): register with reactive forms

--- src/app/modules/auth/pages/register/register.component.ts
+++ src/app/modules/auth/pages/register/register.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
+  formRegister: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.formRegister = this.fb.group({
+      fullname: ['', [Validators.required]],
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+      confirmPassword: ['', [Validators.required]],
+    });
   }
 
+  register() {
+    if (this.formRegister.valid) {
+      console.log(this.formRegister.value);
+    }
+  }
 }
