ebd0544833c963f679bde4b13f6f618cfd82386e
Lionel Acosta <lionelx87@gmail.com>
2023-05-21 19:45:26 -0300

    feat(register): refactor in register method

--- src/app/modules/auth/services/auth.service.ts
+++ src/app/modules/auth/services/auth.service.ts
@@ -4,15 +4,15 @@ import {
   createUserWithEmailAndPassword,
   updateProfile,
 } from '@angular/fire/auth';
-import { getAuth, UserCredential } from '@firebase/auth';
-import { Observable, catchError, from, map, of, switchMap, tap } from 'rxjs';
+import { getAuth } from '@firebase/auth';
+import { Observable, catchError, from, map, of, switchMap } from 'rxjs';
 import { RegisterUser } from 'src/app/core/models/user.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(public auth: Auth) {
+  constructor(private auth: Auth) {
     this.auth = getAuth();
   }
 
@@ -22,10 +22,9 @@ export class AuthService {
       createUserWithEmailAndPassword(this.auth, username, password)
     );
     return registeredUser$.pipe(
-      switchMap((userCredential: UserCredential) => {
-        const { user } = userCredential;
-        return from(updateProfile(user, { displayName })).pipe(map(() => true));
-      }),
+      switchMap(({ user }) =>
+        from(updateProfile(user, { displayName })).pipe(map(() => true))
+      ),
       catchError(err => of(false))
     );
   }
