59d292bdb02e0d409243ce75b8fbf9dfae6d9104
Lionel Acosta <lionelx87@gmail.com>
2023-05-22 23:34:45 -0300

    test(app): first test success

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
   preset: 'jest-preset-angular',
   globalSetup: 'jest-preset-angular/global-setup',
   collectCoverage: true,
-  coverageDirectory: 'coverage/youlist'
+  coverageDirectory: 'coverage/youlist',
+  setupFilesAfterEnv: ['<rootDir>/src/setupJest.ts'],
 };
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,33 +1,34 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('AppComponent', () => {
-  // beforeEach(async () => {
-  //   await TestBed.configureTestingModule({
-  //     imports: [RouterTestingModule],
-  //     declarations: [AppComponent],
-  //   }).compileComponents();
-  // });
-  // it('should create the app', () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   const app = fixture.componentInstance;
-  //   expect(app).toBeTruthy();
-  // });
-  // it(`should have as title 'youlist'`, () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   const app = fixture.componentInstance;
-  //   expect(app.title).toEqual('youlist');
-  // });
-  // it('should render title', () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   fixture.detectChanges();
-  //   const compiled = fixture.nativeElement as HTMLElement;
-  //   expect(compiled.querySelector('.content span')?.textContent).toContain(
-  //     'youlist app is running!'
-  //   );
-  // });
-  it('test', () => {
-    return undefined;
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [],
+      declarations: [AppComponent],
+      providers: [],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
+    });
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
   });
 });
+
+// it(`should have as title 'youlist'`, () => {
+//   //   const fixture = TestBed.createComponent(AppComponent);
+//   //   const app = fixture.componentInstance;
+//   //   expect(app.title).toEqual('youlist');
+//   // });
