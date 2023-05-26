eb17e12c8aeba4e2b76cfe5c4de0941bcd97dfd7
Lionel Acosta <lionelx87@gmail.com>
2023-05-26 01:11:36 -0300

    feat(global): spinner with effects

--- src/app/shared/components/spinner/spinner.component.ts
+++ src/app/shared/components/spinner/spinner.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable, map } from 'rxjs';
+import { AppState } from 'src/app/store/app.reducer';
 
 @Component({
   selector: 'app-spinner',
   templateUrl: './spinner.component.html',
   styleUrls: ['./spinner.component.scss'],
 })
-export class SpinnerComponent {
-  constructor() {}
+export class SpinnerComponent implements OnInit {
+  show$: Observable<boolean>;
+
+  constructor(private store: Store<AppState>) {}
+
+  ngOnInit(): void {
+    this.show$ = this.store.select('ui').pipe(map(({ loading }) => loading));
+  }
 }
--- src/app/store/auth/auth.effects.ts
+++ src/app/store/auth/auth.effects.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { catchError, delay, map, mergeMap, of, repeat, take, tap } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { catchError, finalize, map, mergeMap, of, tap } from 'rxjs';
 import { AuthService } from 'src/app/modules/auth/services/auth.service';
+import { CustomMessageService } from 'src/app/shared/services/custom-message.service';
+import { AppState } from '../app.reducer';
+import { hideLoading, showLoading } from '../ui/ui.actions';
 import {
   registerUser,
   registerUserFail,
   registerUserSuccess,
 } from './auth.actions';
-import { CustomMessageService } from 'src/app/shared/services/custom-message.service';
-import { MessageService } from 'primeng/api';
 
 @Injectable({
   providedIn: 'root',
@@ -17,18 +19,21 @@ export class AuthEffects {
   constructor(
     private actions$: Actions,
     private authService: AuthService,
-    private customMessageService: CustomMessageService
+    private customMessageService: CustomMessageService,
+    private store: Store<AppState>
   ) {}
 
   registerUser$ = createEffect(() =>
     this.actions$.pipe(
       ofType(registerUser),
+      tap(() => this.store.dispatch(showLoading())),
       mergeMap(action =>
         this.authService.register(action.user).pipe(
           map(success =>
             success ? registerUserSuccess() : registerUserFail()
           ),
-          catchError(err => of(registerUserFail()))
+          catchError(err => of(registerUserFail())),
+          finalize(() => this.store.dispatch(hideLoading()))
         )
       )
     )
