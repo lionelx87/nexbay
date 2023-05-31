7fb3eda428cb1e9befe6a751c0d0e7a7d1d1b8df
Lionel Acosta <lionelx87@gmail.com>
2023-05-30 23:45:21 -0300

    feat(guard): remove can activate

--- src/app/shared/guards/auth.guard.ts
+++ src/app/shared/guards/auth.guard.ts
@@ -1,13 +1,5 @@
 import { Injectable } from '@angular/core';
-import {
-  ActivatedRouteSnapshot,
-  CanActivate,
-  CanLoad,
-  Route,
-  RouterStateSnapshot,
-  UrlSegment,
-  UrlTree,
-} from '@angular/router';
+import { CanLoad, Route, UrlSegment } from '@angular/router';
 import { Observable, map, take } from 'rxjs';
 import { CustomRoute } from 'src/app/core/models/routing.interface';
 import { CustomRoutingService } from 'src/app/core/services/custom-routing.service';
@@ -16,22 +8,12 @@ import { AuthService } from 'src/app/modules/auth/services/auth.service';
 @Injectable({
   providedIn: 'root',
 })
-export class AuthGuard implements CanActivate, CanLoad {
+export class AuthGuard implements CanLoad {
   constructor(
     private authService: AuthService,
     private customRoutingService: CustomRoutingService
   ) {}
 
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ):
-    | Observable<boolean | UrlTree>
-    | Promise<boolean | UrlTree>
-    | boolean
-    | UrlTree {
-    return true;
-  }
   canLoad({ path: route }: Route, segments: UrlSegment[]): Observable<boolean> {
     return this.authService.isAuth().pipe(
       take(1),
