46d6875e6b00d589fde5c96451b22320bb4e94dd
Lionel Acosta <lionelx87@gmail.com>
2023-07-02 17:49:14 -0300

    feat(list): rename classes

--- src/app/modules/list/list.module.ts
+++ src/app/modules/list/list.module.ts
@@ -6,9 +6,11 @@ import { MenuModule } from 'src/app/shared/components/layouts/menubar/menubar.mo
 import { ListComponent } from './components/list/list.component';
 import { ListRoutingModule } from './list-routing.module';
 import { ListPageComponent } from './pages/list-page/list-page.component';
+import { ButtonModule } from 'primeng/button';
+import { RippleModule } from 'primeng/ripple';
 
 @NgModule({
   declarations: [ListPageComponent, ListComponent],
-  imports: [CommonModule, ListRoutingModule, MenuModule, OrderListModule],
+  imports: [CommonModule, ListRoutingModule, MenuModule, OrderListModule, ButtonModule, RippleModule],
 })
 export class ListModule {}
