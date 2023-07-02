3583a5bba8cd68ff7cd6a77103a23db7105738f7
Lionel Acosta <lionelx87@gmail.com>
2023-07-02 16:44:21 -0300

    feat(list): create list mocked

---
+++ src/app/modules/list/components/list/list.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { ItemState, List, ListState } from 'src/app/shared/models/list.interface';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.scss']
+})
+export class ListComponent implements OnInit {
+
+  list: List = {
+    id: null,
+    name: 'prueba',
+    items: [
+      { id: '1', description: 'Item 1', state: ItemState.PENDING},
+      { id: '2', description: 'Item 2', state: ItemState.PENDING},
+      { id: '3', description: 'Item 3', state: ItemState.PENDING},
+      { id: '4', description: 'Item 4', state: ItemState.PENDING},
+      { id: '5', description: 'Item 5', state: ItemState.PENDING},
+      { id: '6', description: 'Item 6', state: ItemState.PENDING},
+      { id: '7', description: 'Item 7', state: ItemState.PENDING},
+      { id: '8', description: 'Item 8', state: ItemState.PENDING},
+      { id: '9', description: 'Item 9', state: ItemState.PENDING},
+      { id: '10', description: 'Item 10', state: ItemState.PENDING},
+    ],
+    state: ListState.EMPTY
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+    console.log('');
+  }
+
+}
--- src/app/modules/list/list.module.ts
+++ src/app/modules/list/list.module.ts
@@ -1,12 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { OrderListModule } from 'primeng/orderlist';
 import { MenuModule } from 'src/app/shared/components/layouts/menubar/menubar.module';
+import { ListComponent } from './components/list/list.component';
 import { ListRoutingModule } from './list-routing.module';
 import { ListPageComponent } from './pages/list-page/list-page.component';
 
 @NgModule({
-  declarations: [ListPageComponent],
-  imports: [CommonModule, ListRoutingModule, MenuModule],
+  declarations: [ListPageComponent, ListComponent],
+  imports: [CommonModule, ListRoutingModule, MenuModule, OrderListModule],
 })
 export class ListModule {}
