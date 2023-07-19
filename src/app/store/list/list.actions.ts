28a6be413e1c67886ea9c70b00469568bd30cdb6
Lionel Acosta <lionelx87@gmail.com>
2023-07-18 23:31:37 -0300

    feat(list): create text for empty states

--- src/app/store/list/list.actions.ts
+++ src/app/store/list/list.actions.ts
@@ -1,5 +1,4 @@
 import { createAction, props } from "@ngrx/store";
-import { List } from "src/app/shared/models/list.interface";
 
 export const createDraft = createAction(
     '[List] Create Draft',
